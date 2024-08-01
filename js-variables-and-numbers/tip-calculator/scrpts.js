704e087f428b9d400240a0d9d8bc533af3285676
Juan Diaz <juandiazc@yahoo.de>
2024-08-01 12:53:53 +0200

    solution-tip-calculator

---
+++ js-variables-and-numbers/tip-calculator/scrpts.js
@@ -0,0 +1,7 @@
+const mealCost = 50;
+const tipPercentage = 0.2;
+
+const tipAmount = mealCost * tipPercentage;
+const totalCost = mealCost + tipAmount;
+
+console.log("The total cost of your bill is:" + totalCost + "euros");
