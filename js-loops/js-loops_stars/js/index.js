cc7c229eea86c24f7397dea8bd49c635e577a3c3
Juan Diaz <juandiazc@yahoo.de>
2024-08-12 17:40:22 +0200

    solution js-loops_stars

--- js-loops/js-loops_stars/js/index.js
+++ js-loops/js-loops_stars/js/index.js
@@ -2,13 +2,25 @@ console.clear();
 
 const starContainer = document.querySelector('[data-js="star-container"]');
 
-function renderStars() {
+function renderStars(filledStars = 0) {
   // reset the star container before rerendering stars
   starContainer.innerHTML = "";
 
   //--v-- your code here --v--
+  for (let i = 1; i <= 5; i++) {
+    const star = document.createElement("img");
+    if (i <= filledStars) {
+      star.src = "assets/star-filled.svg";
+    } else {
+      star.src = "assets/star-empty.svg";
+    }
+    star.addEventListener("click", () => {
+      renderStars(i);
+    });
 
+    starContainer.append(star);
+  }
   //--^-- your code here --^--
 }
 
-renderStars();
+renderStars(0);
