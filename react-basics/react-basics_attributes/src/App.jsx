143f8de086ea8aba81a317afc44744dc04bc4b3d
Judiaca <juandiazc@yahoo.de>
2024-08-23 16:56:44 +0200

    Update the label element in App.jsx

--- react-basics/react-basics_attributes/src/App.jsx
+++ react-basics/react-basics_attributes/src/App.jsx
@@ -4,7 +4,7 @@ function Article() {
   return (
     <article className="article">
       <h2 className="article_title">Moin</h2>
-      <label htmlFor="name"> Your name</label>
+      <label htmlFor="some-input"> Your name</label>
       <input id="some-input" type="text" />
       <a href="https://en.wikipedia.org/wiki/Moin" className="article__link">
         Moin, moi or mojn
