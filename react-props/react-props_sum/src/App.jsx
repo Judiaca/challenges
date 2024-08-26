a45f7e10944087905c722a7040e1dee886da1593
Juan Diaz <juandiazc@yahoo.de>
2024-08-26 12:29:24 +0200

    final solution react-props_sum

--- react-props/react-props_sum/src/App.jsx
+++ react-props/react-props_sum/src/App.jsx
@@ -1,15 +1,15 @@
 import "./App.css";
 
+function Sum({ valueA, valueB }) {
+  const sum = valueA + valueB;
+
+  return (
+    <h1>
+      {valueA} + {valueB} = {sum}
+    </h1>
+  );
+}
+
 export default function App() {
   return <Sum valueA={12} valueB={10} />;
 }
-
-function Sum({ valueA, valueB }) {
-  const sum = valueA + valueB;
-}
-
-return (
-  <h1>
-    {valueA} + {valueB} = {sum}
-  </h1>;
-)
