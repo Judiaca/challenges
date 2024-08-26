32d4e353d4733832c5767cc03ad6ec7189ecfb28
Juan Diaz <juandiazc@yahoo.de>
2024-08-26 11:36:27 +0200

    solution react-props_greeting

--- react-props/react-props_greeting/src/App.jsx
+++ react-props/react-props_greeting/src/App.jsx
@@ -1,5 +1,22 @@
 import "./App.css";
 
+// Define the Greeting component
+function Greeting({ name }) {
+  // Conditional rendering: check if the name is one of the coaches
+  if (
+    name === "Klaus" ||
+    name === "Sven" ||
+    name === "Jan" ||
+    name === "Gimena"
+  ) {
+    return <h1>Hello, Coach!</h1>;
+  }
+
+  // Default greeting
+  return <h1>Hello, {name}!</h1>;
+}
+
 export default function App() {
-  return <h1>Replace me with your component!</h1>;
+  // Replace the default heading with the Greeting component ands pass a name prop
+  return <Greeting name="Horst" />;
 }
